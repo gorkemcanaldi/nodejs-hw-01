ed4287f07e8c29603df99dcec67a3e3eba28011a
görkem can aldı <gorkem.aldi2003@gmail.com>
2025-09-16 22:37:17 +0300

    implement generateContacts and getAllContacts

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,13 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const existingContacts = await readContacts();
+  const newContacts = createFakeContact();
+  const allContacts = existingContacts.concat(newContacts);
+  await writeContacts(allContacts);
+  console.log('1 adet yeni iletişim bilgisi eklendi.');
+};
 
 addOneContact();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.error('yazma hatası:', error.message);
+  }
+};

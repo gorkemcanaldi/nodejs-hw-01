d491a09edd5bddc387994dd4b69a7ab29ac57160
görkem can aldı <gorkem.aldi2003@gmail.com>
2025-09-16 22:49:17 +0300

    complete all contact management scripts: generate, add, get, count, remove last, remove all

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (contacts.length === 0) {
+    console.log('Silinecek contact yok.');
+    return;
+  }
+  contacts.pop();
+  await writeContacts(contacts);
+  console.log('Son iletişim bilgisi silindi.');
+};
 
 removeLastContact();
